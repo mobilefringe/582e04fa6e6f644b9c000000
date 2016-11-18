68829f375877f905280ae2638271393ad27cea8e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-11-18 16:38:55 -0500

    Upper Canada Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -1,1 +1,34 @@
 /*Created 2015-02-28 by CodeCloud Team*/
+
+function renderBanner(home_banner_template, home_banner, banners){
+    var item_list = [];
+    var item_rendered = [];
+    var banner_template_html = $(home_banner_template).html();
+    Mustache.parse(banner_template_html);   // optional, speeds up future uses
+    $.each(banners, function(key, val) {
+        today = new Date();
+        start = new Date (val.start_date);
+       
+        start.setDate(start.getDate());
+        if(val.url == "" || val.url === null){
+           val.css = "style=cursor:default;";
+           val.noLink = "return false";
+        }
+        if (start <= today){
+            if (val.end_date){
+                end = new Date (val.end_date);
+                end.setDate(end.getDate() + 1);
+                if (end >= today){
+                    item_list.push(val);  
+                }
+            } else {
+                item_list.push(val);
+            }
+        }
+    });
+    $.each( item_list , function( key, val ) {
+        var repo_rendered = Mustache.render(banner_template_html, val);
+        item_rendered.push(repo_rendered);
+    });
+    $(home_banner).html(item_rendered.join(''));
+}
