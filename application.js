0de2943831d11cd3e8d991aaa19bce4633bdddd3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-11-21 16:01:00 -0500

    Upper Canada Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -98,8 +98,8 @@ function renderHours(container, template, collection, type){
     if (type == "holiday_hours") {
         $.each( collection , function( key, val ) {
             if (!val.store_id && val.is_holiday == true) {
-                holiday = moment(val.holiday_date).tz(getPropertyTimeZone());
-                val.formatted_date = in_my_time_zone(holiday, "MMM D");
+                holiday = moment(val.holiday_date).tz(getPropertyTimeZone()).format("MMM D");
+                // val.formatted_date = in_my_time_zone(holiday, "MMM D");
                 if (val.open_time && val.close_time && val.is_closed == false){
                     var open_time = moment(val.open_time).tz(getPropertyTimeZone());
                     var close_time = moment(val.close_time).tz(getPropertyTimeZone());
