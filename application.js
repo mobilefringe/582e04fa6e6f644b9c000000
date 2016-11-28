e633e5e301f9716ef2a758a59bcdc017d49d4c48
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-11-28 09:35:15 -0500

    Upper Canada Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -489,10 +489,10 @@ function renderHomeHours(container, template, collection){
     Mustache.parse(template_html);   // optional, speeds up future uses
     item_list.push(collection);    
     $.each(item_list, function(key, val) {
-        val.day = get_day(val.day_of_week);
+        val.day = moment().date();
         var d = moment();
-        val.month = get_month(d.getMonth());
-        val.weekday = addZero(d.getDate());
+        val.month = moment().month();
+        val.weekday = moment().day();
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = moment(val.open_time).tz(getPropertyTimeZone());
             var close_time = moment(val.close_time).tz(getPropertyTimeZone());
