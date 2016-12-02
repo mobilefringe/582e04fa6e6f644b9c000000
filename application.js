b887fd4870972302316ea9b360a433647111c149
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-12-02 09:25:44 -0500

    Upper Canada Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -307,7 +307,7 @@ function renderJobs(container, template, collection){
     Mustache.parse(template_html); 
     $.each( collection , function( key, val ) {
         if(val.jobable_type == "Store"){
-            val.store_name = store_name;
+            val.store_name = val.store_name;
             val.store_slug = getStoreDetailsByID(val.jobable_id).slug;
         }
         else{
